3a1c1afcc3b5065a8f7357053947013609f32fd1
Crowdbotics <team@crowdbotics.com>
2021-10-08 15:42:39 +0000

    Update admin title

--- backend/disciplefirst_12969/urls.py
+++ backend/disciplefirst_12969/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "AGWM"
-admin.site.site_title = "AGWM Admin Portal"
-admin.site.index_title = "AGWM Admin"
+admin.site.site_header = "AGWM - obsolete"
+admin.site.site_title = "AGWM - obsolete Admin Portal"
+admin.site.index_title = "AGWM - obsolete Admin"
 
 # swagger
 schema_view = get_schema_view(
